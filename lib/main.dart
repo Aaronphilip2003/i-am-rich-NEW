43b720ff8010edd0bcbc22ac21e22b14d1f0878f
Aaron Philip <aaronphilip2003@gmail.com>
2022-01-06 09:18:39 +0530

    The first commit
    scsc
    
    .
    quit()

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+// import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          backgroundColor: Colors.purple,
+          title: Text("I am Rich", style: TextStyle(color: Color(0xFF000000))),
+        ),
+        bottomNavigationBar: BottomAppBar(),
+        body: Center(
+          child: Image(
+            image: AssetImage('images/diamond.png'),
+          ),
+        ),
+      ),
+    ),
+  );
+}
